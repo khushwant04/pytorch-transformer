b4098e785dadb66cbc60f00108cc5d86969ea0fc
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-09-26 12:42:55 +0000

    commit

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from sympy import false
 import torch 
 import torch.nn as nn
 import math 
@@ -15,3 +16,31 @@ class InputEmbedding(nn.Module):
         return self.embedding(x) * math.sqrt(self.d_model)   
     
 
+class PostionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len , d_model)
+        # Create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Appy cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term)
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)  
+        return self.dropout(x)
+          
+        
