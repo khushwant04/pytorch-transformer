2ee58fa6ef73c92ab5979726a93c4019e72787bb
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-10-13 12:36:21 +0000

    commit

--- model.py
+++ model.py
@@ -1,62 +1,42 @@
-from sympy import false
+import math
 import torch 
-import torch.nn as nn
-import math 
-
+import torch.nn as nn 
+import torch.nn.functional as F 
 
 class InputEmbedding(nn.Module):
     
     def __init__(self, d_model: int, vocab_size: int):
-        super(InputEmbedding, self).__init__()
-        self.d_model = d_model 
-        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.vocab_size  = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
-        
+    
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)   
-    
 
-class PostionalEncoding(nn.Module):
+
+class PostitionalEncoding(nn.Module):
     
     def __init__(self, d_model: int, seq_len: int, dropout: float):
-        super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
+        # Create a matrix of size (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
         
-        # Create a matrix of shape (seq_len, d_model)
-        pe = torch.zeros(seq_len , d_model)
-        # Create a vector of shape (seq_len)
+        # Create a matrix of size (seq_len, 1)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        # Create a vector of shape (d_model)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
-        # Apply sine to even indices
+        
+        # Create the div term with the vector shape d_model
+        div_term = torch.exp(torch.arange(0, d_model, 2)).float() * (-math.log(10000.0)/ d_model)
+        # apply the postional encoding to the even terms
         pe[:, 0::2] = torch.sin(position * div_term)
-        # Appy cosine to odd indices
-        pe[:, 1::2] = torch.cos(position * div_term)
-        # Add a batch dimension to the positional encoding
-        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
-        # Register the positional encoding as a buffer
+        # apply the positional encoding to the odd terms
+        pe[:, 0::1] = torch.cos(position * div_term)
+        # Register the positional encoding as buffer
         self.register_buffer('pe', pe)
-        
-    def forward(self, x):
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)  
-        return self.dropout(x)
     
-          
-class LayerNormalization(nn.Module):
-    
-    def __init__(self, features: int, eps: float = 10**-6):
-        super().__init__()
-        self.eps = eps 
-        self.alpha = nn.Parameter(torch.ones(features))
-        self.bias = nn.Parameter(torch.zeros(features))
-        
     def forward(self, x):
-        # x: (batch, seq_len, hidden_size)
-        # keep the dimension for broadcasting 
-        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
-        # keep the dimenstion for broadcasting 
-        std = x.std(dim=-1, keepdim=True)
-        # eps is to prevent dividing by zero or when std is very small
-        return self.alpha * (x - mean) / (std + self.eps) + self.bias            
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x) 
+                
+             
