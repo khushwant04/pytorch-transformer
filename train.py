d037b42b6d363b9668ff4c7a5440630b7e99299f
khushwantsanwalot2004@outlook.com <khushwant04>
2024-11-04 17:36:15 +0000

    commit

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch 
 import torch.nn as nn 
-
+from torch.utils.data import Dataset, DataLoader, random_split
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -11,7 +11,6 @@ from pathlib import Path
 def get_all_sentences(ds, lang):
     for item in ds:
         yield item['translation'][lang]
-
 
 
 def get_or_build_tokenizer(config, ds, lang):
@@ -29,4 +28,13 @@ def get_or_build_tokenizer(config, ds, lang):
     return tokenizer        
 
 def get_ds(config):
-    ds_row = load_dataset("")    
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+    
+    # Build tokenizer
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+    
+    # Keep 90% for training and 10% for validation
+    train_ds_size = int(0.9 * len(ds_raw)) 
+    val_ds_size = len(ds_raw) - train_ds_size  
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
